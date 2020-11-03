cdc887949c85a85d73092174e4463f9ac60f8cd4
Santiago Nunez-Corrales <snunezcr@gmail.com>
2020-11-03 14:12:31 -0600

    Elixir boilerplate

---
+++ lib/specabm.ex
@@ -0,0 +1,18 @@
+defmodule Specabm do
+  @moduledoc """
+  Documentation for `Specabm`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Specabm.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/specabm_test.exs
@@ -0,0 +1,8 @@
+defmodule SpecabmTest do
+  use ExUnit.Case
+  doctest Specabm
+
+  test "greets the world" do
+    assert Specabm.hello() == :world
+  end
+end
